fd76153e600fb09c51f3a94e3cc1a1754f147cb9
Per Christian B. Viken <per.christian.bechstrom.viken@hogia.se>
2023-05-29 11:39:01 +0200

    refactor(lib): Use file-scoped namespaces

--- TypeContractor/Helpers/PathHelpers.cs
+++ TypeContractor/Helpers/PathHelpers.cs
@@ -1,71 +1,70 @@
-﻿using System.Text;
+using System.Text;
 
-namespace TypeContractor.Helpers
+namespace TypeContractor.Helpers;
+
+internal static class PathHelpers
 {
-    internal static class PathHelpers
+    public static string RelativePath(string targetTypeName, string relativeToTypeName)
     {
-        public static string RelativePath(string targetTypeName, string relativeToTypeName)
+        var targetPath = targetTypeName
+            .Replace('.', Path.DirectorySeparatorChar)
+            .Split(Path.DirectorySeparatorChar)
+            .ToList();
+
+        var relativePath = relativeToTypeName
+            .Replace('.', Path.DirectorySeparatorChar)
+            .Split(Path.DirectorySeparatorChar)
+            .ToList();
+
+        if (targetPath.SequenceEqual(relativePath))
+            return ".";
+
+        return RelativePathDiff(targetPath.ToArray(), relativePath.ToArray());
+    }
+
+    private static string RelativePathDiff(string[] absDirs, string[] relDirs)
+    {
+        // Get the shortest of the two paths
+        int len = absDirs.Length < relDirs.Length ? absDirs.Length : relDirs.Length;
+
+        // Use to determine where in the loop we exited
+        int lastCommonRoot = -1;
+        int index;
+
+        // Find common root
+        for (index = 0; index < len; index++)
         {
-            var targetPath = targetTypeName
-                .Replace('.', Path.DirectorySeparatorChar)
-                .Split(Path.DirectorySeparatorChar)
-                .ToList();
-
-            var relativePath = relativeToTypeName
-                .Replace('.', Path.DirectorySeparatorChar)
-                .Split(Path.DirectorySeparatorChar)
-                .ToList();
-
-            if (targetPath.SequenceEqual(relativePath))
-                return ".";
-
-            return RelativePathDiff(targetPath.ToArray(), relativePath.ToArray());
+            if (absDirs[index] == relDirs[index]) lastCommonRoot = index;
+            else break;
         }
 
-        private static string RelativePathDiff(string[] absDirs, string[] relDirs)
+        // If we didn't find a common prefix then throw
+        if (lastCommonRoot == -1)
         {
-            // Get the shortest of the two paths
-            int len = absDirs.Length < relDirs.Length ? absDirs.Length : relDirs.Length;
-
-            // Use to determine where in the loop we exited
-            int lastCommonRoot = -1;
-            int index;
-
-            // Find common root
-            for (index = 0; index < len; index++)
-            {
-                if (absDirs[index] == relDirs[index]) lastCommonRoot = index;
-                else break;
-            }
-
-            // If we didn't find a common prefix then throw
-            if (lastCommonRoot == -1)
-            {
-                throw new ArgumentException("Paths do not have a common base");
-            }
-
-            // Build up the relative path
-            var relativePath = new StringBuilder();
-
-            // Add on the ..
-            for (index = lastCommonRoot + 1; index < relDirs.Length; index++)
-            {
-                if (relDirs[index].Length > 0) relativePath.Append("../");
-            }
-
-            // If there are no dots, go the other way
-            if (relativePath.Length == 0)
-            {
-                relativePath.Append("./");
-            }
-
-            // Add on the folders
-            for (index = lastCommonRoot + 1; index <= absDirs.Length - 1; index++)
-            {
-                relativePath.Append(absDirs[index] + "/");
-            }
-
-            return relativePath.ToString();
+            throw new ArgumentException("Paths do not have a common base");
         }
+
+        // Build up the relative path
+        var relativePath = new StringBuilder();
+
+        // Add on the ..
+        for (index = lastCommonRoot + 1; index < relDirs.Length; index++)
+        {
+            if (relDirs[index].Length > 0) relativePath.Append("../");
+        }
+
+        // If there are no dots, go the other way
+        if (relativePath.Length == 0)
+        {
+            relativePath.Append("./");
+        }
+
+        // Add on the folders
+        for (index = lastCommonRoot + 1; index <= absDirs.Length - 1; index++)
+        {
+            relativePath.Append(absDirs[index] + "/");
+        }
+
+        return relativePath.ToString();
     }
 }
