1649fffb061629e433879959c2afc0fd80d09b2e
Per Christian B. Viken <per.christian.bechstrom.viken@hogia.se>
2023-09-02 11:41:20 +0200

    feat(lib): Add simple logging capabilities

---
+++ TypeContractor/Logger/ILog.cs
@@ -0,0 +1,10 @@
+namespace TypeContractor.Logger;
+
+public interface ILog
+{
+    void LogDebug(string message);
+    void LogError(Exception exception, string message);
+    void LogError(string message);
+    void LogMessage(string message);
+    void LogWarning(string message);
+}
---
+++ TypeContractor/Logger/NullLogger.cs
@@ -0,0 +1,24 @@
+namespace TypeContractor.Logger;
+
+public class NullLogger : ILog
+{
+    public void LogDebug(string message)
+    {
+    }
+
+    public void LogError(Exception exception, string message)
+    {
+    }
+
+    public void LogError(string message)
+    {
+    }
+
+    public void LogMessage(string message)
+    {
+    }
+
+    public void LogWarning(string message)
+    {
+    }
+}
